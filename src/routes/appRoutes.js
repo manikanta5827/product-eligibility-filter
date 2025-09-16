f3c82d1f680a0b13b9bc8e22ecdd9471ec3f9bf3
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-16 17:03:52 +0530

    update: use multer middleware

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 const appController = require('../controllers/appController.js');
 
 // routes
 router.post('/offer', appController.createOffer);
-router.post('/leads/upload', appController.uploadleads);
+router.post('/leads/upload', upload.single('file'), appController.uploadleads);
 router.post('/score', appController.runScores);
 router.get('/results', appController.getResults);
 
